38636cd6457316441c6339a96ba31073bd8c1759
rattrapper <galka.dima@gmail.com>
2015-04-04 11:18:11 +0300

    The beginning of something

--- Matrices/Matrix.c
+++ Matrices/Matrix.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdio.h>
+#include <malloc.h>
 
 #define TElem double
 
@@ -6,3 +8,19 @@ typedef struct {
 	TElem **data;
 	int base1, base2;
 } Matrix;
+
+Init(Matrix m)
+{
+	m.data = (TElem**)malloc(sizeof(TElem*) * m.base1);
+	for (int i = 0; i < m.base1; i++)
+		m.data[i] = (TElem*)malloc(sizeof(TElem) * m.base2);
+	return 0;
+}
+TElem Get(Matrix m, int row, int col)
+{
+	if (row < 0 || row > m.base1)
+		fprintf(stderr, "argument out of range (row)");
+	else if (col < 0 || col > m.base1)
+		fprintf(stderr, "argument out of range (col)");
+	return m.data[row][col];
+}
